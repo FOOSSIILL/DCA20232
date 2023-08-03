3e20b178598e285333a1d25899f856d03affa19a
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-08-03 12:52:24 -0500

    feat: Styles and Index.html and Index.js

---
+++ Index.js
@@ -0,0 +1,88 @@
+
+const var1 = "Hola";
+const var2 = 5;
+const var3 = true;
+const var4 = [1,2,3];
+const var5 = null;
+let var6
+
+const myNumber = 42;
+const result = myNumber + "1";
+console.log('holi',result);
+
+console.log(typeof null);
+
+if(typeof var1 === "number"){
+    console.log('holaaa');
+}else{
+    console.log('Error');
+}
+
+/*const myBooleanResult = typeof var1 === "string" ?
+console.log('holi') : console.log('error');
+
+if(typeof var1 === "string"){
+    myBooleanResult = true;
+}else{
+    console.log('Error');
+}*/
+
+/*if (condicion1) {
+    //Hago algo
+}else if (condicion2) {
+    //Hago algo
+} else {
+
+}
+
+switch (var1) {
+    case typeof var1 === "number":
+        //Hago algo
+        break;
+        case typeof var1 === "string":
+        //Hago algo
+        break;
+        case typeof var1 === "string":
+        //Hago algo
+        break;
+}*/
+
+for (let i = 0; counter <=5; counter++) {
+
+}
+
+for (let index = 0; index < array.length; index++) {
+    
+    
+}
+
+const myArray = [1,2,3];
+
+myArray.forEach(result => {
+    console.log(result);
+});
+
+myArray.map(result =>{
+    return console.log(result + 1);
+})
+
+//Objects
+
+const myObject = [
+    {
+        nombre: 'Pedrito',
+        age: '12'
+    },
+    {
+        nombre: 'Marta',
+        age: 14
+    },
+    {
+        nombre: 'Andres',
+        age: 15
+    }
+]
+
+myObject.forEach(Object => {
+    console.log(Object.name);
+})
